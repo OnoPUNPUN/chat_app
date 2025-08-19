12ba1992868c59ae08e79eaee58c220775f9630d
OnoPUNPUN <punpunonodera754@gmail.com>
2025-08-19 20:04:58 +0600

    completed login page(UI)

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,8 +1,18 @@
+import 'package:chat_app/widgets/my_buttons.dart';
 import 'package:chat_app/widgets/my_textfeild.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
-  const LoginScreen({super.key});
+
+  // Email & passowrd controller
+  final TextEditingController _emailTEcontroller = TextEditingController();
+  final TextEditingController _passwordTEcontroller = TextEditingController();
+
+
+  LoginScreen({super.key});
+
+  //login method
+  void login() {}
 
   @override
   Widget build(BuildContext context) {
@@ -33,13 +43,35 @@ class LoginScreen extends StatelessWidget {
 
             const SizedBox(height: 25,),
             // email textfeild
-            MyTextfeild(),
-        
+            MyTextfeild(hintText: 'Email', obscureText: false, controller: _emailTEcontroller,),
+
+            const SizedBox(height: 10,),
+            
             // password textfeild
+            MyTextfeild(hintText: 'Password', obscureText: true, controller: _passwordTEcontroller,),
+
+            const SizedBox(height: 25,),
         
             // login button
+            MyButtons(buttonTitle: 'Login', onTap: login,),
+
+            const SizedBox(height: 25,),
         
             // register now
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text("Not a member?", style: TextStyle(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                ),
+                Text(" Register now", style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
